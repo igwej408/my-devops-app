ac7647cfef23124cfbb75f40be4264e75937808f
igwej408 <igwej408@gmail.com>
2026-06-02 17:33:42 +0100

    changed the version to 3

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from my CI/CD Pipeline! Version 1.0.0 🚀');
-   res.send('Hello from my CI/CD Pipeline! Version 2.0.0 🚀');
+  res.send('Hello from my CI/CD Pipeline! Version 3 🚀');
+
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
